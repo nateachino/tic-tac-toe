b39a396a559602b2d866c4eb90a68a7d00357553
nateachino <natealleyne88@gmail.com>
2022-11-05 17:25:22 -0400

    refactored displayController and added winning

--- game.js
+++ game.js
@@ -1,107 +1,158 @@
-let displayController = (() => {
-  const gameBoard = document.getElementById("board");
+const gameBoard = document.getElementById("board");
+
+function PlayerData(marker, turn, win) {
+  this.marker = marker;
+  this.turn = turn;
+  this.win = win;
+}
+
+const playerOne = new PlayerData("X", true, false);
+const playerTwo = new PlayerData("O", false, false);
+
+let createBoard = (() => {
   var squareArray = [];
 
-  var playerOne = true;
-  var playerTwo = false;
-  // const playerOne = player("X");
-  // const playerTwo = player("O");
   for (i = 0; i < 9; i++) {
     const square = document.createElement("div");
     let squareObj = {
-      position: i,
       marker: null,
-      row: null,
-      column: null,
-      diagonal: null,
-      middle: null,
       taken: false,
     };
-    var position = squareObj.position;
-
-    if (position == 0 || position == 1 || position == 2) {
-      squareObj.row = 1;
-    } else if (position == 3 || position == 4 || position == 5) {
-      squareObj.row = 2;
-    } else if (position == 6 || position == 7 || position == 8) {
-      squareObj.row = 3;
-    }
-
-    if (position == 0 || position == 3 || position == 6) {
-      squareObj.column = 1;
-    } else if (position == 1 || position == 4 || position == 7) {
-      squareObj.column = 2;
-    } else if (position == 2 || position == 5 || position == 8) {
-      squareObj.column = 3;
-    }
-
-    if (position == 0 || position == 5 || position == 8) {
-      squareObj.diagonal = 1;
-    } else if (position == 2 || position == 5 || position == 6) {
-      squareObj.diagonal = 2;
-    }
-
-    if (position == 5) {
-      squareObj.middle = true;
-    }
 
     squareArray.push(squareObj);
+  }
+  return squareArray;
+})();
 
-    square.classList.add("square" + i + 1);
+function check() {
+  if (playerOne.turn == true) {
+    playerOne.turn = false;
+    playerTwo.turn = true;
+  } else {
+    playerOne.turn = true;
+    playerTwo.turn = false;
+  }
+
+  let winner = checkWin();
+
+  console.log(winner);
+  if (winner == playerOne.marker) {
+    playerOne.win = true;
+    playerTwo.win = false;
+    console.log(playerOne);
+  } else if (winner == playerTwo.marker) {
+    playerTwo.win = true;
+    playerOne.win = false;
+    console.log(playerTwo);
+  }
+}
+
+function checkWin() {
+  let squareArray = createBoard;
+
+  if (squareArray[0].marker != null) {
+    if (
+      squareArray[0].marker == squareArray[1].marker &&
+      squareArray[0].marker == squareArray[2].marker
+    ) {
+      return squareArray[0].marker;
+    }
+  }
+
+  if (squareArray[3].marker != null) {
+    if (
+      squareArray[3].marker == squareArray[4].marker &&
+      squareArray[3].marker == squareArray[5].marker
+    ) {
+      return squareArray[3].marker;
+    }
+  }
+
+  if (squareArray[6].marker != null) {
+    if (
+      squareArray[6].marker == squareArray[7].marker &&
+      squareArray[6].marker == squareArray[8].marker
+    ) {
+      return squareArray[6].marker;
+    }
+  }
+
+  if (squareArray[0].marker != null) {
+    if (
+      squareArray[0].marker == squareArray[3].marker &&
+      squareArray[0].marker == squareArray[6].marker
+    ) {
+      return squareArray[0].marker;
+    }
+  }
+
+  if (squareArray[1].marker != null) {
+    if (
+      squareArray[1].marker == squareArray[4].marker &&
+      squareArray[1].marker == squareArray[7].marker
+    ) {
+      return squareArray[1].marker;
+    }
+  }
+
+  if (squareArray[2].marker != null) {
+    if (
+      squareArray[2].marker == squareArray[5].marker &&
+      squareArray[2].marker == squareArray[8].marker
+    ) {
+      return squareArray[2].marker;
+    }
+  }
+
+  if (squareArray[0].marker != null) {
+    if (
+      squareArray[0].marker == squareArray[4].marker &&
+      squareArray[0].marker == squareArray[8].marker
+    ) {
+      return squareArray[0].marker;
+    }
+  }
+
+  if (squareArray[2].marker != null) {
+    if (
+      squareArray[2].marker == squareArray[4].marker &&
+      squareArray[2].marker == squareArray[6].marker
+    ) {
+      return squareArray[2].marker;
+    }
+  }
+}
+
+let displayController = (() => {
+  let squareArray = createBoard;
+  if (squareArray) {
+  }
+  squareArray.forEach((element, index) => {
+    const square = document.createElement("div");
+    square.classList.add("square");
     square.style.border = "black 1px solid";
     gameBoard.append(square);
 
     square.addEventListener("click", () => {
-      if (squareObj.taken == false) {
-        if (playerOne == true) {
-          squareObj.marker = "X";
-          square.style.backgroundColor = "blue";
-          playerOne = false;
-          playerTwo = true;
+      if (element.taken == false) {
+        if (playerOne.turn == true) {
+          if (playerOne.win == true || playerTwo.win == true) {
+          } else {
+            element.taken = true;
+            element.marker = playerOne.marker;
+            square.innerHTML = playerOne.marker;
+            check();
+          }
         } else {
-          squareObj.marker = "O";
-          square.style.backgroundColor = "red";
-          playerOne = true;
-          playerTwo = false;
-        }
-        square.innerHTML = squareObj.marker;
-        squareObj.taken = true;
-        console.log(place(squareObj.position));
-      }
-    });
-
-    const place = (pos) => {
-      var rowNum = 0;
-      var columnNum = 0;
-      const squareObject = squareArray[pos];
-      const row = squareObject.row;
-      const column = squareObject.column;
-      const marker = squareObject.marker;
-
-      var row1 = {
-        marker: "",
-      };
-      var row2 = {
-        marker: "",
-      };
-      var row3 = {
-        marker: "",
-      };
-
-      squareArray.forEach((obj) => {
-        if (obj.row == row) {
-          if (marker == obj.marker) {
-            row1.marker += marker;
-            return row1;
+          if (playerOne.win == true || playerTwo.win == true) {
+          } else {
+            element.taken = true;
+            element.marker = playerTwo.marker;
+            square.innerHTML = playerTwo.marker;
+            check();
           }
         }
-      });
-    };
-  }
-
-  return squareArray;
+      }
+    });
+  });
 })();
-
-let turn = (player) => {
-  return marker;
-};
